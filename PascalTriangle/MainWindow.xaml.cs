e50aced18e0a6c8576d5e47baffe6acfc9e0bdfb
Intezium <Intezium04@gmail.com>
2018-03-17 18:21:21 +0300

    Проект завершен

--- PascalTriangle/MainWindow.xaml.cs
+++ PascalTriangle/MainWindow.xaml.cs
@@ -1,28 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PascalTriangle
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int triangleHeight;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ClearField()
+        {
+            Grid_Field.Children.Clear();
+            Grid_Field.RowDefinitions.Clear();
+            Grid_Field.ColumnDefinitions.Clear();
+        }
+
+        private void Create_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Label[][] field = new Label[triangleHeight + 1][];
+
+            ClearField();
+
+            for (int y = 0; y <= triangleHeight; y++)
+            {
+                Grid_Field.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+
+                field[y] = new Label[y + 1];
+
+                for (int x = 0; x <= y; x++)
+                {
+                    Label newLabel = new Label();
+
+                    Grid_Field.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+                    
+                    field[y][x] = newLabel;
+                    field[y][0].Content = "1";
+
+                    Grid_Field.Children.Add(newLabel);
+
+                    Grid.SetRow(newLabel, y);
+                    Grid.SetColumn(newLabel, x);
+                }
+
+                field[y][field[y].Length - 1].Content = "1";
+            }
+
+            for (int y = 1; y < field.GetLength(0); y++)
+                for (int x = 1; x < field[y].Length - 1; x++)
+                {
+                    int a = int.Parse(field[y - 1][x - 1].Content.ToString());
+                    int b = int.Parse(field[y - 1][x].Content.ToString());
+
+                    field[y][x].Content = a + b;
+                }
+        }
+
+        private void ComboBox_TriangleHeight_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem comboBoxItem = (ComboBoxItem)ComboBox_TriangleHeight.SelectedItem;
+
+            triangleHeight = int.Parse(comboBoxItem.Content.ToString());
         }
     }
 }
